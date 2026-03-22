d6d05291204887ddd6bf6b166f9daa42621360f2
Manfred Nilsson <manfred.nilsson@pilotfish.se>
2026-03-22 16:40:41 +0100

    wip

--- main.go
+++ main.go
@@ -17,70 +17,83 @@ func main() {
 	a := app.New()
 	w := a.NewWindow("Touch Game")
 	w.Resize(fyne.NewSize(800, 600))
-	w.SetFullScreen(true)
 
 	content := container.NewWithoutLayout()
 	w.SetContent(content)
 
+	// Dark background
+	bg := canvas.NewRectangle(color.RGBA{R: 30, G: 30, B: 30, A: 255})
+	bg.Resize(fyne.NewSize(800, 600))
+	content.Add(bg)
+
+	// Score
 	score := 0
 	scoreText := canvas.NewText("Score: 0", color.White)
 	scoreText.TextSize = 24
 	scoreText.Move(fyne.NewPos(20, 20))
 	content.Add(scoreText)
 
-	rand.Seed(time.Now().UnixNano())
-
-	// Spawn 1-5 circles every second
 	go func() {
 		for range time.Tick(time.Second) {
-			numCircles := 1 + rand.Intn(5)
-			for i := 0; i < numCircles; i++ {
-				// Capture coordinates and radius locally
+			num := 1 + rand.Intn(3)
+			for i := 0; i < num; i++ {
+				radius := float32(20 + rand.Intn(30))
 				x := float32(rand.Intn(800))
 				y := float32(rand.Intn(600))
-				radius := float32(20 + rand.Intn(15))
 
+				// Circle
 				circle := canvas.NewCircle(color.RGBA{
-					R: uint8(rand.Intn(256)),
-					G: uint8(rand.Intn(256)),
-					B: uint8(rand.Intn(256)),
+					R: uint8(50 + rand.Intn(206)),
+					G: uint8(50 + rand.Intn(206)),
+					B: uint8(50 + rand.Intn(206)),
 					A: 255,
 				})
 				circle.Resize(fyne.NewSize(radius*2, radius*2))
 				circle.Move(fyne.NewPos(x-radius, y-radius))
 
-				// Wrap circle in a transparent button to get Tapped behavior
-				btn := widget.NewButton("", func(cx, cy, r float32, c *canvas.Circle, b *widget.Button) func() {
-					return func() {
-						score += 3
-						content.Remove(c)
-						content.Remove(b)
-						scoreText.Text = fmt.Sprintf("Score: %d", score)
-						scoreText.Refresh()
-					}
-				}(x, y, radius, circle, nil)) // btn passed later
-
+				// Transparent button for tap
+				btn := widget.NewButton("", nil)
 				btn.Resize(fyne.NewSize(radius*2, radius*2))
 				btn.Move(fyne.NewPos(x-radius, y-radius))
-				btn.Importance = widget.LowImportance // hide button visuals
+				btn.Importance = widget.LowImportance
 
-				content.Add(circle)
-				content.Add(btn)
-				canvas.Refresh(circle)
+				// Tap behavior
+				btn.OnTapped = func() {
+					score += 1
+					fyne.Do(func() {
+						content.Remove(circle)
+						content.Remove(btn)
+						scoreText.Text = fmt.Sprintf("Score: %d", score)
+						scoreText.Refresh()
+					})
+				}
 
-				// Animate growth then remove
-				go func(c *canvas.Circle, b *widget.Button, r, px, py float32) {
+				// Add to container on UI thread
+				fyne.Do(func() {
+					content.Add(circle)
+					content.Add(btn)
+					circle.Refresh()
+					content.Refresh()
+				})
+
+				// Animate growth
+				go func(c *canvas.Circle, b *widget.Button, px, py, r float32) {
 					time.Sleep(time.Second)
-					r *= 2
-					c.Resize(fyne.NewSize(r*2, r*2))
-					c.Move(fyne.NewPos(px-r, py-r))
-					b.Resize(fyne.NewSize(r*2, r*2))
-					b.Move(fyne.NewPos(px-r, py-r))
-					canvas.Refresh(c)
+					fyne.Do(func() {
+						r *= 2
+						c.Resize(fyne.NewSize(r*2, r*2))
+						c.Move(fyne.NewPos(px-r, py-r))
+						b.Resize(fyne.NewSize(r*2, r*2))
+						b.Move(fyne.NewPos(px-r, py-r))
+						c.Refresh()
+						content.Refresh()
+					})
 					time.Sleep(time.Second)
-					content.Remove(c)
-					content.Remove(b)
-				}(circle, btn, radius, x, y)
+					fyne.Do(func() {
+						content.Remove(c)
+						content.Remove(b)
+					})
+				}(circle, btn, x, y, radius)
 			}
 		}
 	}()
